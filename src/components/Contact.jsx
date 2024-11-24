13ba03c81f36ef105f6e591f9936ba0074cd6b1f
ntshuxekom98@gmail.com <ntshuxekom98@gmail.com>
2024-11-24 14:20:43 +0200

    first commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+import { toast } from "react-toastify";
+import { motion } from "framer-motion";
+
+const Contact = () => {
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "fca4af2b-5945-42f2-9a48-6ae25910bd01");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("");
+      toast.success("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      toast.error(data.message);
+      setResult("");
+    }
+  };
+
+  return (
+    <motion.div
+      initial={{ opacity: 0, x: -200 }}
+      transition={{ duration: 1 }}
+      whileInView={{ opacity: 1, x: 0 }}
+      viewport={{ once: true }}
+      className="text-center p-6 py-20 lg:px-32 w-full overflow-hidden"
+      id="Contact"
+    >
+      <h1 className="text-2xl sm:text-4xl font-bold mb-2">
+        Contact
+        <span className="underline underlin-offset-4 decoration-1 under font-light">
+          With Us
+        </span>
+      </h1>
+      <p className="text-center text-gray-500 mb-12 max-w-80 mx-auto">
+        Want to Have the Home of Your Dreams? Get in Touch with Us
+      </p>
+
+      <form
+        onSubmit={onSubmit}
+        className="max-w-2xl mx-auto text=gray-600 pt-8"
+      >
+        <div className="flex flex-wrap">
+          <div className="w-full md:w-1/2 text-left">
+            Your Name
+            <input
+              className="w-full border border-gray-300 rounded py-3 px-4 mt-2"
+              type="text"
+              name="Name"
+              placeholder="Type your name..."
+              required
+            />
+          </div>
+          <div className="w-full md:w-1/2 text-left md:pl-4">
+            Your Email
+            <input
+              className="w-full border border-gray-300 rounded py-3 px-4 mt-2"
+              type="email"
+              name="Email"
+              placeholder="Type your email..."
+              required
+            />
+          </div>
+        </div>
+
+        <div className="my-6 text-left">
+          Message
+          <textarea
+            className="w-full border border-gray-300 rounded py-3 px-4 mt-2 h-48 resize-none"
+            name="Message"
+            placeholder="Type your message..."
+            required
+          ></textarea>
+        </div>
+
+        <button className="bg-blue-600 text-white py-2 px-12 mb-10 rounded">
+          {result ? result : "Send Message"}
+        </button>
+      </form>
+    </motion.div>
+  );
+};
+
+export default Contact;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./index.html", // Ensure this includes your HTML file
+    "./src/**/*.{js,jsx,ts,tsx}", // Ensure this includes all JS/JSX/TS/TSX files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
